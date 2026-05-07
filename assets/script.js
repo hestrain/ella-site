490eb3cf060f323df2999510e0c8bd79956adb2a
hestrain <hestrain@gmail.com>
2026-05-07 17:22:05 -0400

    yay i fixed the ways the plays generate, images exist, all good. need to add real descriptions and fix the formatting and then its basically done

--- assets/script.js
+++ assets/script.js
@@ -3,55 +3,55 @@ let currentPlay = ""
 
 //array of play objects
 //(1)fireproof, (2)wolf, (3)corps, (4)care/take, (5)becca & the blob, (6)exits
-const plays = [
+const unsortedPlays = [
     {
         title: "exits",
-        title2:"Exits",
+        title2: "Exits",
         order: 6,
         description:
             "EXITS DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-            pic:"./photos/exits.jpg"
+        pic: "./assets/photos/exits.jpg",
     },
     {
         title: "wolf",
-        title2:"Wolf",
+        title2: "Wolf",
         order: 2,
         description:
             "WOLF DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-            pic:"./photos/Wolf2.jpg"
+        pic: "./assets/photos/Wolf2.jpg",
     },
     {
         title: "corps",
-        title2:"Corps",
-        order:3,
+        title2: "Corps",
+        order: 3,
         description:
             "CORPS DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-            pic:"./corps.jpg"
+        pic: "./assets/photos/corps.jpg",
     },
     {
         title: "fireproof",
-        title2:"Fireproof",
+        title2: "Fireproof",
         order: 1,
         description:
             "FIREPROOF DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-            pic:"./fireproof.jpg"
+        pic: "./assets/photos/fireproof.jpg",
     },
     {
         title: "beccablob",
-        title2:"Becca and the Blob",
+        title2: "Becca and the Blob",
         order: 5,
         description:
             "BECCABLOB DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-            pic:"./photos/beccablob.jpg"
+        pic: "./assets/photos/beccablob.jpg",
     },
     {
         title: "caretake",
-        title2:"Care/Take",
+        title2: "Care/Take",
         order: 4,
         description:
             "CARE/TAKE DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-            pic:"./photos/caretake.jpg"
-    }
+        pic: "./assets/photos/caretake.jpg",
+    },
 ];
 
 //function to show the play description when the cover is hovered over. x is from onmouseenter event
@@ -78,7 +78,7 @@ function showDesc(x) {
 
     //set description variable to the play desription (and converts that to string)
     desc = filteredData.description.toString();
-;
+    ;
     //sets currentPlay to the right element (so that we can hide it easier in the next function)
     currentPlay = document.getElementById(playId + "-desc");
 
@@ -105,34 +105,68 @@ function hideDesc() {
 const playsDiv = document.getElementById("playbox")
 console.log(playsDiv);
 
-function myFunction(){
+function myFunction() {
+
+const plays = unsortedPlays.sort((a, b) => a.order - b.order);
 
     plays.forEach(play => {
-        console.log(play.title);
-        
+        //log the title of the play
+        console.log(play.title + "'s card is being made");
+
+        //create the card element
         const playCard = document.createElement("div")
         playCard.setAttribute("class", "play")
-        //setAttribute("id", play)
-        
+        //title element
         const playTitle = document.createElement("h4")
         playTitle.innerHTML = play.title2
+        //cover div element
         const coverDiv = document.createElement("div")
-        coverDiv.setAttribute("class", "cover $(title)")
+        coverDiv.setAttribute("class", "cover")
         //.onmouseenter("showDesc(this)").onmouseleave("hideDesc()").addAttribute("play-id", title)
-        const coverImg = document.createElement("div")
+        //cover image element
+        const coverImg = document.createElement("img")
         coverImg.setAttribute("class", "sample-img smaller-img")
-        
         coverImg.setAttribute("class", play.title);
+        coverImg.setAttribute("src", play.pic);
+
+        //description element (nothing in it rn)
         const descDiv = document.createElement("div")
-        descDiv.setAttribute("class","play-desc-desc")
-        
-        playCard.appendChild(playTitle); 
+        descDiv.setAttribute("class", "play-desc-desc")
+        descDiv.innerHTML = play.description
+
+        //adds all the elements together onto the playcard, and then to the playsDiv
+        playCard.appendChild(playTitle);
         playCard.appendChild(coverDiv)
         coverDiv.appendChild(coverImg)
         playCard.appendChild(descDiv)
         playsDiv.append(playCard);
-        
-        
+
+
+        //give playcard the event listener to trigger the description showing
+        playCard.addEventListener("mouseenter", (event) => {
+            // currently mouseover makes text purple
+            event.target.style.color = "purple";
+            playCard.append(descDiv)
+            coverImg.style.opacity = "0%"
+
+        })
+        playCard.addEventListener("mouseleave", (event) => {
+            // and mouseleave makes text red
+            event.target.style.color = "red";
+            playCard.removeChild(descDiv)
+                        coverImg.style.opacity = "100%";
+
+        }
+        )
+
+        //testing picture retrieval
+        // const testImg = document.createElement("img")
+        // testImg.setAttribute("class", play.title)
+        // testImg.setAttribute("src", play.pic)
+        // testImg.setAttribute("style", "width:200px")
+
+        // playsDiv.append(testImg)
+
     });
 }
 

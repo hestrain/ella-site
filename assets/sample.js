4f7d877273eabdddef316e962c0422e446812c45
hestrain <hestrain@gmail.com>
2026-05-08 23:13:10 -0400

    trying t o move pplays to a json file lol

--- assets/sample.js
+++ assets/sample.js
@@ -1,15 +1,12 @@
-//array of play objects
-//(1)fireproof, (2)wolf, (3)corps, (4)care/take, (5)becca & the blob, (6)exits
 
-const unsortedPlays = [
-  {
-    title: "exits",
-    title2: "Exits",
-    order: 99,
-    description: `<scan class="quote">"What if people don't like me for good reasons?"</scan><br><br>
+exports.array = [{
+  title: "exits",
+  title2: "Exits",
+  order: 99,
+  description: `<scan class="quote">"What if people don't like me for good reasons?"</scan><br><br>
     Rachel has run away from home with nothing but her school bag and a well-read copy of Moby Dick. Anxious, closeted, and being followed around by an apparition of her younger self that only she can see, Rachel is at the end of her rope. But when she meets Sam, an older girl doing graffiti in an alleyway downtown, she's convinced she's found her saviour. The only play about a 17-year-old lesbian who's obsessed with a 625-page novel about whaling, Exits is a comedic, dramatic, nerdy, whimsical exploration of queer identity and growing up. <br>`,
-    pic: "./assets/photos/exits2.jpg",
-    otherInfo: `Cast size: 3F <br>
+  pic: "./assets/photos/exits2.jpg",
+  otherInfo: `Cast size: 3F <br>
     TYA 14+ <br><br>
     Developed as part of Playwrights Workshop Montreal Young Creators Unit, Dramaturgy by Jesse Stong <br><br>
     Development history:
@@ -48,7 +45,7 @@ const unsortedPlays = [
         Ella Kohlmann and Madie Jolliffe of Exits</a>
     </li>
   </ul>`,
-  },
+},
   {
     title: "wolf",
     title2: "Wolf",
@@ -61,8 +58,8 @@ const unsortedPlays = [
     for something she cannot name. Charlotte wants to try and rekindle things with Iris, but Iris is much more
     interested in the famous painter that Charlotte is assisting while he does a residency in town— and who sees the
     hunger in Iris that that she is becoming increasingly unable to hold back.`,
-    pic: "./assets/photos/Wolf2.jpg",
-    otherInfo: `Cast size: 5 (3F/2M)<br>
+      pic: "./assets/photos/Wolf2.jpg",
+        otherInfo: `Cast size: 5 (3F/2M)<br>
     Work-in-progress <br>
     Developed at the National Theatre School of Canada, dramaturgy by Andrea Romaldi
     <br><br>
@@ -76,19 +73,19 @@ const unsortedPlays = [
     </li>
   </ul>`,
   },
-  {
-    title: "corps",
+{
+  title: "corps",
     title2: "Corps",
-    order: 10,
-    description: `<scan class="quote">Four women meet in a conference room for a company-mandated "female empowerment workshop." In an
+      order: 10,
+        description: `<scan class="quote">Four women meet in a conference room for a company-mandated "female empowerment workshop." In an
       office down the hall, the disembodied spirit of their recently-deceased HR director, tells the woman who was hired
       to replace her that she should quit while she's ahead. </scan><br><br>
     A darkly comic social thriller in the vein of Get Out and Severance, Corps is a workplace haunted house story that
     explores lateral violence. What happens when we feel unable to affect the people and systems that keep us oppressed,
     and instead direct our anger at those closest to us? In a time where the most vulnerable of us are more
     desperate—and more divided— than ever, Corps asks: how do we keep ourselves safe?`,
-    pic: "./assets/photos/corps.jpg",
-    otherInfo: `Cast size: 6F<br>
+          pic: "./assets/photos/corps.jpg",
+            otherInfo: `Cast size: 6F<br>
     Work-in-progress<br>
     Ongoing development with Playwrights Workshop Montreal, dramaturgy by Sarah Elkashef<br>
     Initially developed as part of Teesri Duniya Theatre's Fireworks Play Development Unit<br><br>
@@ -110,28 +107,28 @@ const unsortedPlays = [
     </li>
   </ul>`,
   },
-  {
-    title: "fireproof",
+{
+  title: "fireproof",
     title2: "Fireproof",
-    order: 1,
-    description: `Description Forthcoming<br><br>
+      order: 1,
+        description: `Description Forthcoming<br><br>
     Set to premiere at the NTS New Words Festival in April 2027`,
-    pic: "./assets/photos/fireproof.jpg",
-    otherInfo: `Cast size: 6 (4F/2M)<br>
+          pic: "./assets/photos/fireproof.jpg",
+            otherInfo: `Cast size: 6 (4F/2M)<br>
   Work-in-progress<br>
   Currently in development at the National Theatre School of Canada, dramaturgy by Jovanni Sy`,
   },
-  {
-    title: "beccablob",
+{
+  title: "beccablob",
     title2: "Becca and the Blob",
-    order: 98,
-    description: `<scan class="quote">"I don't understand humans at all..."</scan> <br><br>
+      order: 98,
+        description: `<scan class="quote">"I don't understand humans at all..."</scan> <br><br>
     When an alien named Blob crash-lands in Becca's backyard, Blob asks for Becca's help tracking down the missing
     pieces of their spaceship. Becca agrees, but only if Blob follows all of Becca's rules and disguises themself as a
     human. A play for ages 6 and up about navigating difference, rules, and social norms; being a good friend; and
     standing up for yourself when people treat you like you're from another planet.`,
-    pic: "./assets/photos/beccablob.jpg",
-    otherInfo: ` (3 performers, 1F/1NB/1ANY)
+          pic: "./assets/photos/beccablob.jpg",
+            otherInfo: ` (3 performers, 1F/1NB/1ANY)
     TYA 6+, work-in-progress<br>
     Currently in development at the National Theatre School of Canada, dramaturgy by Dean Patrick Fleming<br><br>
 
@@ -142,18 +139,18 @@ const unsortedPlays = [
     </li>
   </ul>`,
   },
-  {
-    title: "caretake",
+{
+  title: "caretake",
     title2: "Care/Take",
-    order: 95,
-    description: ` <scan class="quote">"She said she wanted me to be happy."</scan><br><br>
+      order: 95,
+        description: ` <scan class="quote">"She said she wanted me to be happy."</scan><br><br>
     Care/Take follows two adult siblings, Will and Emily, who reunite in their hometown when their father has a stroke
     and is taken to the hospital for emergency surgery. As the two argue about what to do if their father is disabled by
     the stroke, wounds from their past reopen, and they must reckon with the roles— caretaker or victim— that each of
     them has taken on in their family. A one-act, one-room, two-hander that asks “when and how should we give care to
     others, and when and how should we accept it for ourselves?”`,
-    pic: "./assets/photos/caretake.jpg",
-    otherInfo: `Cast size: 2 (1F/1M)<br>
+          pic: "./assets/photos/caretake.jpg",
+            otherInfo: `Cast size: 2 (1F/1M)<br>
     One-act (40min), Production-ready<br>
     Developed at the National Theatre School of Canada, dramaturgy by Ann-Marie Macdonald<br><br>
 
@@ -166,82 +163,5 @@ const unsortedPlays = [
       • Workshop and reading, National Theatre School of Canada, January 2025
     </li>
   </ul>`,
-  },
-];
-
-  
-
-//where the plays go
-const playsDiv = document.getElementById("playbox");
-console.log(playsDiv);
-
-//function that creates the plays
-function populatePlays() {
-  //sort the plays by the order#
-  const plays = unsortedPlays.sort((a, b) => a.order - b.order);
-
-  //for each play in the array....
-  plays.forEach((play) => {
-    //log the title of the play
-    console.log(play.title + "'s card is being made");
-
-    //create the card element
-    const playCard = document.createElement("div");
-    playCard.setAttribute("class", "play");
-    //title element
-    const playTitle = document.createElement("h4");
-    playTitle.innerHTML = play.title2;
-    //cover div element
-    const coverDiv = document.createElement("div");
-    coverDiv.setAttribute("class", "cover");
-    //cover image element
-    const coverImg = document.createElement("img");
-    coverImg.setAttribute("class", "sample-img smaller-img");
-    coverImg.setAttribute("class", play.title);
-    coverImg.setAttribute("src", play.pic);
-
-    //description element
-    const descDiv = document.createElement("div");
-    descDiv.setAttribute("class", "play-desc");
-    descDiv.innerHTML = play.description;
-    descDiv.style.visibility = "hidden";
-
-    const fullDeets = document.createElement("button")
-    fullDeets.setAttribute("class","detail-btn")
-    fullDeets.innerHTML = ("More Info")
-
-    fullDeets.addEventListener("click", (event) => {
-      location.href("/"+title);
-    })
-
-    //adds all the elements together onto the playcard, and then to the playsDiv
-    playCard.appendChild(playTitle);
-    playCard.appendChild(coverDiv);
-    coverDiv.appendChild(coverImg);
-    playCard.appendChild(descDiv);
-    playsDiv.append(playCard);
-    descDiv.style.visibility = "hidden";
-
-    //give playcard the event listener to trigger the description showing when mouse is on card
-    playCard.addEventListener("mouseenter", (event) => {
-      //image opacity to 0 so you cna read desc
-      coverImg.style.opacity = "0%";
-      //shows the desc text
-      descDiv.style.visibility = "visible";
-      //log to check
-      console.log("hovered on " + play.title2);
-    });
-    //when the mouse leaves the playCard
-    playCard.addEventListener("mouseleave", (event) => {
-      //bring the opacity back up
-      coverImg.style.opacity = "100%";
-      //hide the desc
-      descDiv.style.visibility = "hidden";
-      //log to check
-      console.log("mouse left " + play.title2);
-    });
-  });
-}
-
-//calls the function
-populatePlays();
+  }
+]

c7501e5e2b47232b03d8cfec05ef387b4c847cb8
hestrain <hestrain@gmail.com>
2026-05-08 21:23:43 -0400

    the play covers and descriptions are working. added mobile css

---
+++ assets/descriptions.js
@@ -0,0 +1,37 @@
+`<p><scan class="quote">"What if people don’t like me for good reasons?"</scan><br>
+Rachel has run away from home with nothing but her school bag and a well-read copy of Moby Dick. Anxious, closeted, and being followed around by an apparition of her younger self that only she can see, Rachel is at the end of her rope. But when she meets Sam, an older girl doing graffiti in an alleyway downtown, she&#39;s convinced she&#39;s found her saviour. The only play about a 17-year-old lesbian who's obsessed with a 625-page novel about whaling, Exits is a comedic, dramatic, nerdy, whimsical exploration of queer identity and growing up. <br></p>
+
+<p>Cast size: 3F <br>
+TYA 14+ <br><br>
+Developed as part of Playwrights Workshop Montreal Young Creators Unit, Dramaturgy by Jesse Stong <br><br>
+Development history: 
+<ul>
+    <li> Production, Montreal Fringe Festival, 2019
+        <li class="indent"> Directed by Madison Jolliffe
+        </li>
+    </li>
+    <li>Staged Reading, Queer Reading Series, Centaur Theatre, 2019
+<li class="indent">
+Directed by Sarah Segal-Lazar
+</li>
+    </li>
+    <li>
+Workshop, Young Creators Unit, Playwrights Workshop Montreal, 2018
+    <li class="indent">
+Directed by Sarah Segal-Lazar, dramaturgy by Jesse Stong
+    </li>
+    </li>
+</ul>
+<br>
+Press:
+<ul>
+    <li>
+        <a href="https:/web.archive.org/web/20190920021153/https:/montrealtheatrehub.com/2019/06/14/fringe-review-c-a-s-t-s-exits-is-nerdy-timeless-and-utterly-enjoyable">Fringe Review: C.A.S.T.’s “Exits” is Nerdy, Timeless and Utterly Enjoyable</a>
+    </li>
+        <li>
+        <a href="https://scripteditty.wordpress.com/2019/05/31/fringeterview-1-ella-kohlmann-and-madie-jolliffe-of-exits/">Queer Romance is at the Heart of Fringe Play Exits</a>
+    </li>
+        <li>
+        <a href="https://thelinknewspaper.ca/article/queer-romance-is-at-the-heart-of-fringe-play-exits">Fringeterview #1: Ella Kohlmann and Madie Jolliffe of Exits</a>
+    </li>
+</ul> </p>`;
--- assets/script.js
+++ assets/script.js
@@ -1,173 +1,233 @@
-//variable to store the currently viewed play
-let currentPlay = ""
-
 //array of play objects
 //(1)fireproof, (2)wolf, (3)corps, (4)care/take, (5)becca & the blob, (6)exits
 const unsortedPlays = [
-    {
-        title: "exits",
-        title2: "Exits",
-        order: 6,
-        description:
-            "EXITS DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-        pic: "./assets/photos/exits.jpg",
-    },
-    {
-        title: "wolf",
-        title2: "Wolf",
-        order: 2,
-        description:
-            "WOLF DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-        pic: "./assets/photos/Wolf2.jpg",
-    },
-    {
-        title: "corps",
-        title2: "Corps",
-        order: 3,
-        description:
-            "CORPS DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-        pic: "./assets/photos/corps.jpg",
-    },
-    {
-        title: "fireproof",
-        title2: "Fireproof",
-        order: 1,
-        description:
-            "FIREPROOF DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-        pic: "./assets/photos/fireproof.jpg",
-    },
-    {
-        title: "beccablob",
-        title2: "Becca and the Blob",
-        order: 5,
-        description:
-            "BECCABLOB DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-        pic: "./assets/photos/beccablob.jpg",
-    },
-    {
-        title: "caretake",
-        title2: "Care/Take",
-        order: 4,
-        description:
-            "CARE/TAKE DESCRIPTION Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat officiis nulla iste aspernatur repellendus exercitationem, eius at maiores nisi, perferendis quisquam tempore aliquid voluptatum labore aliquam libero dignissimos ducimus numquam.",
-        pic: "./assets/photos/caretake.jpg",
-    },
+  {
+    title: "exits",
+    title2: "Exits",
+    order: 99,
+    description: `<scan class="quote">"What if people don't like me for good reasons?"</scan><br>
+    Rachel has run away from home with nothing but her school bag and a well-read copy of Moby Dick. Anxious, closeted, and being followed around by an apparition of her younger self that only she can see, Rachel is at the end of her rope. But when she meets Sam, an older girl doing graffiti in an alleyway downtown, she's convinced she's found her saviour. The only play about a 17-year-old lesbian who's obsessed with a 625-page novel about whaling, Exits is a comedic, dramatic, nerdy, whimsical exploration of queer identity and growing up. <br>`,
+    pic: "./assets/photos/exits2.jpg",
+    otherInfo: `Cast size: 3F <br>
+    TYA 14+ <br><br>
+    Developed as part of Playwrights Workshop Montreal Young Creators Unit, Dramaturgy by Jesse Stong <br><br>
+    Development history:
+  <ul>
+    <li> Production, Montreal Fringe Festival, 2019
+    <li class="indent"> Directed by Madison Jolliffe
+    </li>
+    </li>
+    <li>Staged Reading, Queer Reading Series, Centaur Theatre, 2019
+    <li class="indent">
+      Directed by Sarah Segal-Lazar
+    </li>
+    </li>
+    <li>
+      Workshop, Young Creators Unit, Playwrights Workshop Montreal, 2018
+    <li class="indent">
+      Directed by Sarah Segal-Lazar, dramaturgy by Jesse Stong
+    </li>
+    </li>
+  </ul>
+  <br>
+  Press:
+  <ul>
+    <li>
+      <a
+        href="https:/web.archive.org/web/20190920021153/https:/montrealtheatrehub.com/2019/06/14/fringe-review-c-a-s-t-s-exits-is-nerdy-timeless-and-utterly-enjoyable">Fringe
+        Review: C.A.S.T.’s “Exits” is Nerdy, Timeless and Utterly Enjoyable</a>
+    </li>
+    <li>
+      <a
+        href="https://scripteditty.wordpress.com/2019/05/31/fringeterview-1-ella-kohlmann-and-madie-jolliffe-of-exits/">Queer
+        Romance is at the Heart of Fringe Play Exits</a>
+    </li>
+    <li>
+      <a href="https://thelinknewspaper.ca/article/queer-romance-is-at-the-heart-of-fringe-play-exits">Fringeterview #1:
+        Ella Kohlmann and Madie Jolliffe of Exits</a>
+    </li>
+  </ul>`,
+  },
+  {
+    title: "wolf",
+    title2: "Wolf",
+    order: 2,
+    description: ` <scan class="quote"> Iris wakes up in the forest with blood in her mouth. How did she get here? When did it begin?
+    </scan> <br>
+    An aspiring painter, Iris has spent the past 4 years stuck in her small hometown working full-time at a
+    manufacturing facility and taking care of her ailing mother. When her ex-girlfriend, Charlotte, returns to town
+    after graduating from the art school that Iris couldn't afford to attend, Iris begins to feel an insatiable hunger
+    for something she cannot name. Charlotte wants to try and rekindle things with Iris, but Iris is much more
+    interested in the famous painter that Charlotte is assisting while he does a residency in town— and who sees the
+    hunger in Iris that that she is becoming increasingly unable to hold back.`,
+    pic: "./assets/photos/Wolf2.jpg",
+    otherInfo: `Cast size: 5 (3F/2M)<br>
+    Work-in-progress <br>
+    Developed at the National Theatre School of Canada, dramaturgy by Andrea Romaldi
+    <br><br>
+    Development history
+  <ul>
+    <li>
+      Workshop and professional reading, National Theatre School of Canada, April 2026
+    </li>
+    <li>
+      Reading, National Theatre School of Canada, February 2026
+    </li>
+  </ul>`,
+  },
+  {
+    title: "corps",
+    title2: "Corps",
+    order: 10,
+    description: `<scan class="quote">Four women meet in a conference room for a company-mandated "female empowerment workshop." In an
+      office down the hall, the disembodied spirit of their recently-deceased HR director, tells the woman who was hired
+      to replace her that she should quit while she's ahead. </scan>
+    A darkly comic social thriller in the vein of Get Out and Severance, Corps is a workplace haunted house story that
+    explores lateral violence. What happens when we feel unable to affect the people and systems that keep us oppressed,
+    and instead direct our anger at those closest to us? In a time where the most vulnerable of us are more
+    desperate—and more divided— than ever, Corps asks: how do we keep ourselves safe?`,
+    pic: "./assets/photos/corps.jpg",
+    otherInfo: `Cast size: 6F<br>
+    Work-in-progress<br>
+    Ongoing development with Playwrights Workshop Montreal, dramaturgy by Sarah Elkashef<br>
+    Initially developed as part of Teesri Duniya Theatre's Fireworks Play Development Unit<br><br>
+
+    Development history:
+  <ul>
+    <li>Week-long workshop with design exploration, staged reading: June 2026
+    <li class="indent">Directed by Rinchen Dolma, dramaturgy by Sarah </li>
+    <li class="indent">Supported by Playwrights Workshop Montreal and The Canada Council for the Arts</li>
+    </li>
+    <li>Two-day workshop: July 2024
+    <li class="indent">Directed by Annie Valentina, dramaturgy by Sarah Elkashef
+    </li>
+    <li class="indent">Supported by Playwrights Workshop Montreal
+    </li>
+    </li>
+    <li>First-draft reading, Teesri Duniya Theatre: May 2023
+    <li class="indent">Directed by Jen Cressey</li>
+    </li>
+  </ul>`,
+  },
+  {
+    title: "fireproof",
+    title2: "Fireproof",
+    order: 1,
+    description: `Description Forthcoming<br>
+    Set to premiere at the NTS New Words Festival in April 2027`,
+    pic: "./assets/photos/fireproof.jpg",
+    otherInfo: `Cast size: 6 (4F/2M)<br>
+  Work-in-progress<br>
+  Currently in development at the National Theatre School of Canada, dramaturgy by Jovanni Sy`,
+  },
+  {
+    title: "beccablob",
+    title2: "Becca and the Blob",
+    order: 98,
+    description: `<scan class="quote">"I don't understand humans at all..."</scan> <br>
+    When an alien named Blob crash-lands in Becca's backyard, Blob asks for Becca's help tracking down the missing
+    pieces of their spaceship. Becca agrees, but only if Blob follows all of Becca's rules and disguises themself as a
+    human. A play for ages 6 and up about navigating difference, rules, and social norms; being a good friend; and
+    standing up for yourself when people treat you like you're from another planet.`,
+    pic: "./assets/photos/beccablob.jpg",
+    otherInfo: ` (3 performers, 1F/1NB/1ANY)
+    TYA 6+, work-in-progress<br>
+    Currently in development at the National Theatre School of Canada, dramaturgy by Dean Patrick Fleming<br><br>
+
+    Development History:
+  <ul>
+    <li>
+      Reading, National Theatre School of Canada, February 2026
+    </li>
+  </ul>`,
+  },
+  {
+    title: "caretake",
+    title2: "Care/Take",
+    order: 95,
+    description: ` <scan class="quote">"She said she wanted me to be happy."</scan><br>
+    Care/Take follows two adult siblings, Will and Emily, who reunite in their hometown when their father has a stroke
+    and is taken to the hospital for emergency surgery. As the two argue about what to do if their father is disabled by
+    the stroke, wounds from their past reopen, and they must reckon with the roles— caretaker or victim— that each of
+    them has taken on in their family. A one-act, one-room, two-hander that asks “when and how should we give care to
+    others, and when and how should we accept it for ourselves?”`,
+    pic: "./assets/photos/caretake.jpg",
+    otherInfo: `Cast size: 2 (1F/1M)<br>
+    One-act (40min), Production-ready<br>
+    Developed at the National Theatre School of Canada, dramaturgy by Ann-Marie Macdonald<br><br>
+
+    Development History:
+  <ul>
+    <li>
+      • Reading, National Theatre School of Canada, April 2025
+    </li>
+    <li>
+      • Workshop and reading, National Theatre School of Canada, January 2025
+    </li>
+  </ul>`,
+  },
 ];
 
-//function to show the play description when the cover is hovered over. x is from onmouseenter event
-function showDesc(x) {
-    //console.log(x); // this was logging the div to mkae sure it was the right place
-
-    //gets the title of the play from the play-id
-    const playId = x.getAttribute("play-id");
-
-    //logs the play name for checking
-    console.log("hovered on " + playId);
-
-    //declare description variable
-    let desc = ""
-
-    //filter through plays array to find the right play to take the description from
-    const filteredData = plays.find(
-        (play) => play.title === playId,
-
-    );
-
-    //log t check filteredData (appears as an array tho)
-    //console.log(filteredData),
-
-    //set description variable to the play desription (and converts that to string)
-    desc = filteredData.description.toString();
-    ;
-    //sets currentPlay to the right element (so that we can hide it easier in the next function)
-    currentPlay = document.getElementById(playId + "-desc");
-
-    //change the descirption element to the description we got from filtreedData
-    currentPlay.innerHTML = desc;
-
-
-    //adds a new class
-    currentPlay.className += " open-desc"
-
-}
-
-//function to hide the description when mouse leaves
-function hideDesc() {
-
-    //clear the description 
-    currentPlay.innerHTML = "";
-    // reset the class names
-    currentPlay.className = "play-desc";
-
-    //log when this action is complete
-    console.log("gone");
-}
-const playsDiv = document.getElementById("playbox")
+//where the plays go
+const playsDiv = document.getElementById("playbox");
 console.log(playsDiv);
 
+//function that creates the plays
 function myFunction() {
+  //sort the plays by the order#
+  const plays = unsortedPlays.sort((a, b) => a.order - b.order);
 
-const plays = unsortedPlays.sort((a, b) => a.order - b.order);
+  //for each play in the array....
+  plays.forEach((play) => {
+    //log the title of the play
+    console.log(play.title + "'s card is being made");
 
-    plays.forEach(play => {
-        //log the title of the play
-        console.log(play.title + "'s card is being made");
+    //create the card element
+    const playCard = document.createElement("div");
+    playCard.setAttribute("class", "play");
+    //title element
+    const playTitle = document.createElement("h4");
+    playTitle.innerHTML = play.title2;
+    //cover div element
+    const coverDiv = document.createElement("div");
+    coverDiv.setAttribute("class", "cover");
+    //cover image element
+    const coverImg = document.createElement("img");
+    coverImg.setAttribute("class", "sample-img smaller-img");
+    coverImg.setAttribute("class", play.title);
+    coverImg.setAttribute("src", play.pic);
 
-        //create the card element
-        const playCard = document.createElement("div")
-        playCard.setAttribute("class", "play")
-        //title element
-        const playTitle = document.createElement("h4")
-        playTitle.innerHTML = play.title2
-        //cover div element
-        const coverDiv = document.createElement("div")
-        coverDiv.setAttribute("class", "cover")
-        //.onmouseenter("showDesc(this)").onmouseleave("hideDesc()").addAttribute("play-id", title)
-        //cover image element
-        const coverImg = document.createElement("img")
-        coverImg.setAttribute("class", "sample-img smaller-img")
-        coverImg.setAttribute("class", play.title);
-        coverImg.setAttribute("src", play.pic);
+    //description element (nothing in it rn)
+    const descDiv = document.createElement("div");
+    descDiv.setAttribute("class", "play-desc");
+    descDiv.innerHTML = play.description;
+    descDiv.style.visibility = "hidden";
 
-        //description element (nothing in it rn)
-        const descDiv = document.createElement("div")
-        descDiv.setAttribute("class", "play-desc-desc")
-        descDiv.innerHTML = play.description
+    //adds all the elements together onto the playcard, and then to the playsDiv
+    playCard.appendChild(playTitle);
+    playCard.appendChild(coverDiv);
+    coverDiv.appendChild(coverImg);
+    playCard.appendChild(descDiv);
+    playsDiv.append(playCard);
+    descDiv.style.visibility = "hidden";
 
-        //adds all the elements together onto the playcard, and then to the playsDiv
-        playCard.appendChild(playTitle);
-        playCard.appendChild(coverDiv)
-        coverDiv.appendChild(coverImg)
-        playCard.appendChild(descDiv)
-        playsDiv.append(playCard);
-
-
-        //give playcard the event listener to trigger the description showing
-        playCard.addEventListener("mouseenter", (event) => {
-            // currently mouseover makes text purple
-            event.target.style.color = "purple";
-            playCard.append(descDiv)
-            coverImg.style.opacity = "0%"
-
-        })
-        playCard.addEventListener("mouseleave", (event) => {
-            // and mouseleave makes text red
-            event.target.style.color = "red";
-            playCard.removeChild(descDiv)
-                        coverImg.style.opacity = "100%";
-
-        }
-        )
-
-        //testing picture retrieval
-        // const testImg = document.createElement("img")
-        // testImg.setAttribute("class", play.title)
-        // testImg.setAttribute("src", play.pic)
-        // testImg.setAttribute("style", "width:200px")
-
-        // playsDiv.append(testImg)
-
+    //give playcard the event listener to trigger the description showing
+    playCard.addEventListener("mouseenter", (event) => {
+      // currently mouseover makes text purple
+      event.target.style.color = "purple";
+      coverImg.style.opacity = "0%";
+      console.log("hovered on " + play.title2);
+      descDiv.style.visibility = "visible";
     });
+    playCard.addEventListener("mouseleave", (event) => {
+      // and mouseleave makes text red
+      event.target.style.color = "red";
+      coverImg.style.opacity = "100%";
+      console.log("mouse left " + play.title2);
+      descDiv.style.visibility = "hidden";
+    });
+  });
 }
 
+//calls the function
 myFunction();
